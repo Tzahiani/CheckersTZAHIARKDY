cfa16b7bba00de0658c7c3e6c0e6adacd9131b61
arklev <acsmasher@gmail.com>
2015-05-23 14:41:29 +0300

    Spkit the main HTML
    To CSS and Java Script Files

---
+++ Offer/script.js
@@ -0,0 +1,41 @@
+var playerScore = 0;
+var AIScore = 0;
+var whiteEat=0;
+var blackEat=0;
+turn=0;
+
+	function move(){
+		
+		var img =$(this);
+
+		img.draggable();
+		img.droppable({
+			drop: function( event, ui ) {
+				$( this ).addClass( "ui-state-highlight" ).find( "td" ).html( "Dropped!" );
+			}
+		
+		}
+
+
+		function start(){
+			$('img').click(move);
+
+		}	
+//This function updates the scoreboard game finish.
+//Human Player = 1
+//AI = 2	
+function UpdateScoreBoard(winner){
+	switch(winner){
+		case 1: 
+			x=Document.getElementById("playerScore");
+			x.innerHTML  = playerScore++;
+			break;
+		case 2:
+			x=Document.getElementById("AIScore");
+			x.innerHTML  = AIScore++;
+		default:
+        	break;
+	}
+}	
+
+	$( document ).ready(start);
