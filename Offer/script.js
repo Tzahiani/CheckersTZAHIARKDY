499e4421a12aa930feaf5a039eb24ee44a0923d8
arklev <acsmasher@gmail.com>
2015-05-24 01:47:22 +0300

    Added ScoreBoard

--- Offer/script.js
+++ Offer/script.js
@@ -1,64 +1,64 @@
 
 var playerScore = 0;
 var AIScore = 0;
-var whiteEat=0;
-var blackEat=0;
-turn=0;
+var whiteEat = 0;
+var blackEat = 0;
+turn = 0;
 var board;
 
-function matrix( rows, cols){
+function matrix(rows, cols) {
 
-	var arr = [];
+    var arr = [];
 
-  // Creates all lines:
-  for(var i=0; i < rows; i++){
+    // Creates all lines:
+    for (var i = 0; i < rows; i++) {
 
-      // Creates an empty line
-      arr.push([]);
+        // Creates an empty line
+        arr.push([]);
 
-      // Adds cols to the empty line:
-      arr[i].push( new Array(cols));
+        // Adds cols to the empty line:
+        arr[i].push(new Array(cols));
 
-      for(var j=0; j < cols; j++){
-        // Initializes:
-        arr[i][j] = $('#'+i+j);
+        for (var j = 0; j < cols; j++) {
+            // Initializes:
+            arr[i][j] = $('#' + i + j);
+        }
+    }
+
+    return arr;
+}
+
+function UpdateScoreBoard(winner) {
+    switch (winner) {
+        case 1:
+            x = Document.getElementById("playerScore");
+            x.innerHTML = playerScore++;
+            break;
+        case 2:
+            x = Document.getElementById("AIScore");
+            x.innerHTML = AIScore++;
+        default:
+            break;
     }
 }
+function print() {
 
-return arr;
+    for (var i = 0; i < 8; i++)
+        for (var j = 0; j < 8; j++)
+            console.log(board[i][j]);
 }
-
-function UpdateScoreBoard(winner){
-	switch(winner){
-		case 1: 
-		x=Document.getElementById("playerScore");
-		x.innerHTML  = playerScore++;
-		break;
-		case 2:
-		x=Document.getElementById("AIScore");
-		x.innerHTML  = AIScore++;
-		default:
-		break;
-	}
-}
-function print(){
-
-	for (var i = 0; i < 8; i++) 
-		for(var j=0;j<8;j++)
-			console.log(board[i][j]);
-	}
 function init() {
-	$('img').draggable();
+    $('img').draggable();
 }
 
-function CheckIfOk(){
+function CheckIfOk() {
     console.log("If you see this then i try to drag some thing.");
 }
-                
-$(document).ready(function(){
-    
-        board = matrix(8,8);
-	print();
-        init();
-        
-  });
+
+$(document).ready(function () {
+
+    board = matrix(8, 8);
+    print();
+    init();
+
+});
